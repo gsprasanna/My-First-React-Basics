eecfea4abf91dd8bff219af33050961d5fd277c5
gsprasanna <erprasanna97@gmail.com>
2019-08-31 14:50:27 +0530

    React basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//const { Component } = React; object destructuring
+// ctrl - space => method which will show present inside it (eg:component).
+//react calls & execute in the order of lifecycle method. 
+// for any class/component, constructor will be called first
+//constructor should have super method  
+class App extends Component{ 
+ constructor(){
+   super();
+
+   debugger;
+   console.log("hello prasanna!");
+ }
+
+ state = {
+   inputValue : "Prasanna"
+ }
+// arrow function and normal function : take the class instance instead of input element instance;
+ onInputChange = (event) => {
+  const inputValue = event.target.value;
+  this.setState({
+    inputValue,
+  })
+ }
+
+ render(){ // render method have dom element in jsx , <> Fragment used to wrap the component
+   debugger;
+   const {inputValue } = this.state;
+  return(    
+    <> 
+      <div>Hello Prasanna!</div>
+      <input value={inputValue} placeholder="add text here" 
+      onChange = {this.onInputChange}
+      />
+    </>
+  )
+ }
+ componentDidMount(){
+  debugger;
+  console.log("component did mount");
+ }
 }
 
 export default App;
